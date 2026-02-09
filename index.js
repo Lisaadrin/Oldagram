b81d8533e7b1bbc97c996cfe86bd8905e28d0311
Naib <naib948@gmail.com>
2026-02-09 12:30:46 +0100

    javascript machinations

---
+++ index.js
@@ -0,0 +1,8 @@
+let oldagram = document.getElementById(main)
+let profilePic = document.getElementById(avatarPoster)
+let userName = document.getElementById(userName)
+let location = document.getElementById(location)
+let mainImg = document.getElementById(mainImage)
+
+
+
